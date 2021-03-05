73333403594b9d10739670823fa8b057f14d361e
Uduak Essien <acushla4real@gmail.com>
2021-03-05 16:17:14 +0100

    Update routes for books

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :categories, only: %i[index create destroy]
+      resources :books, only: %i[index create show update destroy]
     end
   end
 end
