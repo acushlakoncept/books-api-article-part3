e43f971eb88c6c2572d5667c0f4762b079c7212c
Uduak Essien <acushla4real@gmail.com>
2021-03-05 16:12:44 +0100

    Add books controller

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,52 @@
+module Api
+  module V1
+    class BooksController < ApplicationController
+      before_action :set_book, only: %i[update show destroy]
+
+      # GET /books
+      def index
+        @books = Book.all
+        render json: BooksRepresenter.new(@books).as_json
+      end
+
+      # POST /book
+      def create
+        @book = Book.create(book_params)
+
+        if @book.save
+          render json: BookRepresenter.new(@book).as_json, status: :created
+        else
+          render json: @book.errors, status: :unprocessable_entity
+        end
+      end
+
+      # GET /books/:id
+      def show
+        render json: BookRepresenter.new(@book).as_json
+      end
+
+
+      # PUT /books/:id
+      def update
+        @book.update(book_params)
+        head :no_content
+      end
+
+      # DELETE /books/:id
+      def destroy
+        @book.destroy
+        head :no_content
+      end
+
+      private
+
+      def book_params
+        params.permit(:title, :author, :category_id)
+      end
+
+      def set_book
+        @book = Book.find(params[:id])
+      end
+    end
+  end
+end
