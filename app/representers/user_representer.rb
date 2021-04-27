9ebf25df793a281ebeb34890b369b1942a985714
Uduak Essien <acushla4real@gmail.com>
2021-04-27 09:12:59 +0100

    Add user representer

---
+++ app/representers/user_representer.rb
@@ -0,0 +1,18 @@
+class UserRepresenter
+    def initialize(user)
+      @user = user
+    end
+  
+    def as_json
+      {
+        id: user.id,
+        username: user.username,
+        token: AuthenticationTokenService.call(user.id)
+      }
+    end
+  
+    private
+  
+    attr_reader :user
+  end
+  
