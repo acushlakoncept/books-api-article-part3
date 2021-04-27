32bd134be1f78c05045734854f5fadeaa8ec1a5e
Uduak Essien <acushla4real@gmail.com>
2021-04-27 09:11:50 +0100

    Update route with login and signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/Authentication'
   namespace :api do
     namespace :v1 do
       resources :categories, only: %i[index create destroy]
       resources :books, only: %i[index create show update destroy]
+
+      post 'login', to: 'authentication#create'
+      post 'register', to: 'users#create'
     end
   end
 end
