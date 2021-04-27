c5cff9038440f4f4f7e2eadf9203713b8eec8914
Uduak Essien <acushla4real@gmail.com>
2021-04-27 09:13:22 +0100

    Add authentication token service

---
+++ app/services/authentication_token_service.rb
@@ -0,0 +1,29 @@
+class AuthenticationTokenService
+    HMAC_SECRET = Rails.application.secrets.secret_key_base
+    ALGORITHM_TYPE = 'HS256'.freeze
+  
+    def self.call(user_id)
+      exp = 24.hours.from_now.to_i
+      payload = { user_id: user_id, exp: exp }
+      JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
+    end
+  
+    def self.decode(token)
+      JWT.decode token, HMAC_SECRET, true, { algorithm: ALGORITHM_TYPE }
+    rescue JWT::ExpiredSignature, JWT::DecodeError
+      false
+    end
+  
+    def self.valid_payload(payload)
+      !expired(payload)
+    end
+  
+    def self.expired(payload)
+      Time.at(payload['exp']) < Time.now
+    end
+  
+    def self.expired_token
+      render json: { error: 'Expired token! login again' }, status: :unauthorized
+    end
+  end
+  
