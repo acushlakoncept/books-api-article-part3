1b44cb1718da35f3d67ec03377bae284d57b3fb5
Uduak Essien <acushla4real@gmail.com>
2021-06-27 07:05:34 +0100

    Add authenticate request, current_user, payload and invalid_authentication method to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,30 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  def authenticate_request!
+    return invalid_authentication if !payload || !AuthenticationTokenService.valid_payload(payload.first)
+
+    current_user!
+    invalid_authentication unless @current_user
+  end
+
+  def current_user!
+    @current_user = User.find_by(id: payload[0]['user_id'])
+  end
+
+  private
+
+  def payload
+    auth_header = request.headers['Authorization']
+    token = auth_header.split(' ').last
+    AuthenticationTokenService.decode(token)
+  rescue StandardError
+    nil
+  end
+
+  def invalid_authentication
+    render json: { error: 'You will need to login first' }, status: :unauthorized
+  end
 end
